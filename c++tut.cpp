2d54eece2168ee279e0a51551b893173ff6a13ce
Sarah Howick <sarahhowick123@gmail.com>
2024-06-27 14:06:13 -0500

    made c++tut
    This is a new c++tutorial program

---
+++ c++tut.cpp
@@ -0,0 +1,16 @@
+# <iostream>
+
+using namespace std;
+
+int main () {
+    int x = 5; 
+    int y = 4; 
+    y = y + x; 
+    cout << y << endl; 
+    cout << x << endl; 
+
+    cout << "HELLO" << endl;
+    
+    return 0;
+
+}
